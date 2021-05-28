026b5356ae5a85fc458d19f425f8036df79b6568
varun reddy <reddy.varun0007@gmail.com>
2021-05-28 19:28:58 +0530

    removed tag in vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "default" {
     enable_dns_hostnames = true
     tags = {
         Name = "${var.vpc_name}"
-	Owner = "Varun Dopathi Reddy"
+	Owner = "Varun Dopathi "
 	environment = "${var.environment}"
     }
 }
