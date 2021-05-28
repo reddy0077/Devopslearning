6f429c15c9427411be0c76881abd9dc4e5beaacd
varun reddy <reddy.varun0007@gmail.com>
2021-05-28 19:15:03 +0530

    Added dynamodb locking

--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ terraform {
      bucket = "varunterraformbucket"
      key = "terraformNewcode.tfstate"
      region = "us-east-1"
+     dynamodb_table = "varun-terraformcode-locking"
+     encrypt        = true  
  }
  }
 
