343ed36ad93bf65e03320a11092e8dc3ef9011c6
reddy0077 <30363967+reddy0077@users.noreply.github.com>
2021-07-22 13:06:52 +0530

    Update s3bucket.tf

--- s3bucket.tf
+++ s3bucket.tf
@@ -10,15 +10,15 @@ resource "aws_s3_bucket" "varunbuckets1" {
     aws_route_table_association.terraform-public,
   ]
 }
- resource "aws_s3_bucket" "varunbuckets2" {
-  bucket = "kfc-bucket-2"
-  acl    = "private"
+#  resource "aws_s3_bucket" "varunbuckets2" {
+#   bucket = "kfc-bucket-2"
+#   acl    = "private"
 
-  tags = {
-    Name        = "kfc-bucket-2"
-    Environment = "Dev"
-  }
-  depends_on = [
-    aws_route_table_association.terraform-public,
-  ]
-}
+#   tags = {
+#     Name        = "kfc-bucket-2"
+#     Environment = "Dev"
+#   }
+#   depends_on = [
+#     aws_route_table_association.terraform-public,
+#   ]
+# }
