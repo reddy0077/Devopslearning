893d778798665f1d778c07053147ac58e5a21385
reddy0077 <30363967+reddy0077@users.noreply.github.com>
2021-07-22 13:08:11 +0530

    Update dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,9 +1,9 @@
- resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "varun-terraformcode-locking"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+#  resource "aws_dynamodb_table" "terraform_locks" {
+#   name         = "varun-terraformcode-locking"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
